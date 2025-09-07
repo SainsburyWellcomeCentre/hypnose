3577c2ba91d329805752c7e91efa1d98605799cc
vlkuzun <vuzungil@gmail.com>
2025-09-07 18:24:19 +0100

    Modify ConvertPoints for RoiActivity formatting and update schemas with rig dimensions

--- src/Extensions/ConvertPoints.cs
+++ src/Extensions/ConvertPoints.cs
@@ -8,14 +8,17 @@ using OpenCV.Net;
 using DataSchema;
 
 [Combinator]
-[Description("Converts a list of DataSchema.Point2d to an array of OpenCV.Net.Point2f.")]
+[Description("Converts a list of DataSchema.Point2d to an array of OpenCV.Net.Point arrays (contours).")]
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class ConvertPoints
 {
-    public IObservable<OpenCV.Net.Point2f[]> Process(IObservable<List<DataSchema.Point2d>> source)
+    public IObservable<OpenCV.Net.Point[][]> Process(IObservable<List<DataSchema.Point2d>> source)
     {
         return source.Select(PointsList =>
-            PointsList.Select(Point => new OpenCV.Net.Point2f((float)Point.X, (float)Point.Y)).ToArray()
+            new OpenCV.Net.Point[][]
+            {
+                PointsList.Select(Point => new OpenCV.Net.Point((int)Math.Round(Point.X), (int)Math.Round(Point.Y))).ToArray()
+            }
         );
     }
 }
